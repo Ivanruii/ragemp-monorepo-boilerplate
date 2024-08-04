5b5919d1cf1714a9e47d047fbd2ca1e04c0faeab
Iván Ruiz <ivr.644@gmail.com>
2024-08-04 16:15:15 +0200

    Added .gitkeep to server-build and server-files directories

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,6 +6,8 @@ mp.events.add("playerReady", (player) => {
   player.call("client::login::display", [true]);
 });
 
+// This is a simple example of how to handle events, it is not a functional login system. You will have to continue from this starting point.
+
 mp.events.add("server::login::receiveData", (player, username, password) => {
   console.log(
     `Received login data from ${player.name}: ${username}, ${password}`
